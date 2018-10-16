0858ecf7b2f946e4a49fedc910ed9c35b111337a
Murilo Borges de Andrade <murilo.andrade@agrotis.com>
2018-10-16 14:14:42 -0300

    feat: Writing base template to view

--- README.md
+++ README.md
@@ -8,3 +8,10 @@ source ~/your_py3_env/bin/activate
 pip install jedi flake8 autopep8
 ```
 https://github.com/jorgenschaefer/elpy
+
+
+# Suggested by Joao.Serodio:
+- https://atlaskit.atlassian.com/
+- https://atlaskit.atlassian.com/docs/getting-started
+- http://atlaskit-starter.surge.sh/
+- 
--- treinamento/urls.py
+++ treinamento/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pessoas/', include('pessoas.urls'))
+    path('pessoas/', include('pessoas.urls')),
+    path('', RedirectView.as_view(url='/pessoas/'), name='pessoas')
 ]
